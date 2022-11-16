f4b76a0540a8abc577ea2506e99152b0e2ccab05
Michael Bosque <mbosq165@d127.org>
2022-11-16 09:35:07 -0600

    Initial Commit

---
+++ Multiply/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Multiply
+//
+//  Created by Michael C Bosque (Student) on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showResultWhenPressed(_ sender: UIButton) {
+        //MVP Here - Michael
+        
+        
+        
+        //Stretch 2 & 3- Jaden
+        
+    
+        
+        
+        //Stretch 1 - Michael
+        
+        
+        //Stretch 4 - Michael
+        
+    }
+    
+}
+
